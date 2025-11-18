82679f8eba7639bc49f839f9165c2a544e4f7e63
ninotica <ninaleaof@gmail.com>
2025-11-18 11:05:55 -0300

    iniciando a lógica de otimização dos algoritmos

--- src/Disciplinas.py
+++ src/Disciplinas.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from itertools import combinations
 import os
+import unicodedata
 
 class Disciplina:
     disciplinas = []
@@ -32,6 +33,7 @@ class Disciplina:
         self.periodos = periodos
         self.curso = curso
         self.horario = horario
+        self.dias = dias
         self.tem_prova = tem_prova
         self.eh_dificil = False
         self.cor = pre_requerimento_data
@@ -39,8 +41,21 @@ class Disciplina:
         Disciplina.disciplinas.append(self)
 
     @classmethod
+    def set_dias_horarios(self):
+        return {(dia, materia.horario) for materia in self.disciplinas_prova() for dia in materia.dias}
+    
+    @classmethod
+    def blocos_validos(self):
+        set_blocos = set([])
+        # aqui definimos conjuntos de matérias que acontecem nos mesmos dias e horários, como ponto de partida
+        for dia_e_horario in Disciplina.set_dias_horarios():
+            bloco = set([materia for materia in self.disciplinas_prova() if dia_e_horario[0] in materia.dias and dia_e_horario[1] == materia.horario])
+            set_blocos.add(tuple(bloco))
+        return [t1 for t1 in set_blocos if not any(set(t1).issubset(set(t2)) for t2 in set_blocos if t1 != t2)]
+
+    @classmethod
     def disciplinas_prova(self):
-        return [d for d in Disciplina.disciplinas if d.tem_prova]
+        return [d for d in self.disciplinas if d.tem_prova]
 
     def __repr__(self):
         return f"{self.nome}"
@@ -87,16 +102,21 @@ class Grafo:
         return dicionario
     
     def coloracao(self):
-        return {i:i.cor for i in self.elementos}
+        return {i:i.cor for i in self.elementos if i.cor is not None}
     
     def calendario(self):
-        cores = {i.cor for i in self.elementos if i.cor is not None}
+        cores = self.coloracao()
         calendario = {}
         for i in cores:
             calendario[i] = [d for d in self.elementos if d.cor == i]
             return calendario
+
+def limpar_nome(texto):
+    # Normaliza, remove acentos (encode ascii), volta pra string e troca espaço por _
+    return unicodedata.normalize('NFKD', texto).encode('ASCII', 'ignore').decode('utf-8').lower().replace(' ', '_')
     
 def carregar_disciplinas_do_arquivo(caminho_arquivo: str):
+
     """
     Carrega as disciplinas de um arquivo .csv ou .xlsx e as cria
     no escopo global usando a coluna 'nome_variavel'.
@@ -129,7 +149,7 @@ def carregar_disciplinas_do_arquivo(caminho_arquivo: str):
         horario = str(row['horario'])
         nome_variavel = str(row['nome_variavel']).strip()
         tem_prova = bool(row['tem_prova'])
-        
+
         if pd.notna(row['data_prof_pediu']):
             data_prof_pediu = int(row['data_prof_pediu'])
         else:
@@ -179,6 +199,7 @@ def carregar_disciplinas_do_arquivo(caminho_arquivo: str):
         globals()[nome_variavel] = nova_disciplina
 
     print(f"Carregadas {len(Disciplina.disciplinas)} disciplinas.")
+
 ################################################################################
 # Setup Inicial
 ################################################################################
@@ -187,17 +208,11 @@ carregar_disciplinas_do_arquivo(file_path)
 
 nr_dias = 7
 
-solucoes_desejadas = 5
-
-requerimentos_profs = {AR: 1, AL : 2} #chave é a disciplina, valor é o dia. 
-
 alunos_puxando = { # set com listas de todas as disciplinas que o aluno com schedule incomum está puxando
 (AL, AR, MD, LP, CVV, AEDV, EMD),
 (AL, AR, MD, LP, CVV, MFF),
 (AC, AL, MD, LP, CVV, AEDV),
 }
-
-materias_dificeis = {MD, AL}
 
 ################################################################################
 # Criação do Grafo
@@ -205,5 +220,8 @@ materias_dificeis = {MD, AL}
 restricoes = Restricoes(Disciplina.disciplinas_prova(), alunos_puxando)
 grafo = Grafo(Disciplina.disciplinas_prova(), restricoes.restricoes())
 drafo = grafo.dict_format()
-for d in drafo:
-    print("*", d, ":", drafo[d])
+# if __name__ == "__main__":
+#     for d in drafo:
+#         print("*", d, ":", drafo[d])
+
+print(Disciplina.blocos_validos())
--- src/coloracao.py
+++ src/coloracao.py
@@ -1,5 +1,5 @@
-from Disciplinas import grafo, nr_dias, requerimentos_profs, Disciplina, AEDV, OCD, MD, AL, materias_dificeis
-grafo = grafo.dict_format()
+from Disciplinas import grafo, nr_dias, requerimentos_profs, Disciplina, AEDV, OCD, MD, AL
+
 ################################################################################
 # Setup Algoritmo Guloso
 ################################################################################
@@ -20,16 +20,7 @@ def requerimentos_razoaveis(requerimento: dict, grafo:dict) -> bool:
                 return False
     return True
 
-def calendario_from_coloracao(coloracao: dict) -> dict:
-    calendario = {}
-    for disciplina in coloracao:
-        if coloracao[disciplina] not in calendario:
-            calendario[coloracao[disciplina]] = {disciplina}
-        else:
-            calendario[coloracao[disciplina]].add(disciplina)
-    return calendario
-
-def ordem_de_ruindade(grafo, calendario, materias_dificeis):
+def ordem_de_ruindade(grafo, calendario):
     ruindade = 0
     for dia in calendario:
         if (dia - 1) not in calendario:
@@ -48,36 +39,48 @@ def primeiro_disponivel(cores_usadas, cores_disponiveis):
     cores = [cor for cor in cores_disponiveis if cor not in cores_usadas]
     return cores[0] if cores else False
 
-def coloracao_gulosa(grafo, cores, pre_requisitos={}):
-    """Find the greedy coloring of G in the given order.
-    The return value is a dictionary mapping vertices to their colors.
+# def coloracao_gulosa(grafo, cores):
+#     """Find the greedy coloring of G in the given order.
+#     The return value is a dictionary mapping vertices to their colors.
 
-    https://en.wikipedia.org/wiki/Greedy_coloring
-    """
-    coloracao = pre_requisitos
-    for vertice in grafo:
-        if vertice in coloracao:
+#     https://en.wikipedia.org/wiki/Greedy_coloring
+#     """
+#     coloracao = {d: d.cor for d in grafo if d.cor is not None}
+#     for vertice in grafo:
+#         if vertice in coloracao:
+#             continue
+#         used_neighbour_colors = {coloracao[nbr]
+#                                  for nbr in grafo[vertice]
+#                                  if nbr in coloracao}
+#         pd = primeiro_disponivel(used_neighbour_colors, cores)
+#         if not pd:
+#             print("Coloração Falhou!")
+#             return 0
+#         coloracao[vertice] = pd
+#     return coloracao
+
+def coloracao_gulosa(grafo, cores):
+    coloracao_inicial = {d: d.cor for d in grafo.elementos}
+    for vertice in grafo.elementos:
+        if vertice.cor is not None:
             continue
-        used_neighbour_colors = {coloracao[nbr]
-                                 for nbr in grafo[vertice]
-                                 if nbr in coloracao}
+        used_neighbour_colors = {nbr.cor
+                                for nbr in grafo.dict_format()[vertice]
+                                if nbr.cor is not None}
         pd = primeiro_disponivel(used_neighbour_colors, cores)
         if not pd:
             print("Coloração Falhou!")
             return 0
-        coloracao[vertice] = pd
+        vertice.cor = pd
+        coloracao = grafo.coloracao()
+        #reseta as cores iniciais
+    for d in grafo.elementos:
+        d.cor = coloracao_inicial[d]
     return coloracao
-
-# def s
-
-def coloracao_DSatur(grafo, cores, pre_requisitos={}):
-    """https://en.wikipedia.org/wiki/DSatur"""
-    proximo_vertice = list(grafo)[0]
-
 ################################################################################
 # Test Driver
 ################################################################################
 
-coloracao = coloracao_gulosa(grafo, [1, 2, 3, 4, 5, 6, 7], requerimentos_profs )
-print(calendario_from_coloracao(coloracao))
+coloracao = coloracao_gulosa(grafo, [1, 2, 3, 4, 5, 6, 7])
+print((coloracao))
 
