3a7104eccee5cd100a5a9eecec9ecf058c8d729f
ninotica <ninaleaof@gmail.com>
2025-11-20 16:01:46 -0300

    Finalizando implementação do algoritmo de otimização da "ruindade"

--- src/Disciplinas.py
+++ src/Disciplinas.py
@@ -4,10 +4,11 @@ from itertools import combinations
 import os
 import copy
 import unicodedata
+import random
 
 class Disciplina:
     disciplinas = []
-    def __init__(self, nome:str, horario:str, dias:list[str], periodos:list[int], curso:list=[1, 0], tem_prova=True, pre_requerimento_data=None):
+    def __init__(self, nome:str, horario:str, dias:list[str], periodos:list[int], curso:list=[1, 0], tem_prova=True, pre_requerimento_data=None, eh_dificil = False):
         """
         Classe Disciplina
 
@@ -36,9 +37,10 @@ class Disciplina:
         self.horario = horario
         self.dias = dias
         self.tem_prova = tem_prova
-        self.eh_dificil = False
+        self.eh_dificil = eh_dificil
         self.cor = pre_requerimento_data
         self.saturacao = 0
+        self.pode_mudar = True if pre_requerimento_data == None else False
 
         Disciplina.disciplinas.append(self)
 
@@ -48,6 +50,8 @@ class Disciplina:
     def set_cor(self, cor):
         self.cor = cor
 
+    def get_dificil(self):
+        return self.eh_dificil
 
     @classmethod
     def set_dias_horarios(self):
@@ -115,61 +119,116 @@ class Restricoes:
     
     def restricoes(self):
         return self.restricoes_basicas() | self.restrições_adicionais()
-    
-    def to_Vertice(self):
-        restricoes = self.restricoes()
-        vertices = [v.to_Vertice() for v in Disciplina.disciplinas_prova()]
-        restricoes_vert = []
-        for (v, u) in restricoes:
-            for vertice in vertices:
-                if v.nome == vertice.get_label():
-                    v = vertice
-                    break
-            for vertice in vertices:
-                if u.nome == vertice.get_label():
-                    u = vertice
-                    break
-            
-            restricoes_vert.append((v, u))
-
-        return restricoes_vert
 
 class Grafo:
-    def __init__(self, vertices, arestas):
+    def __init__(self, vertices: Disciplina, arestas: Restricoes):
         self.elementos = vertices
-        self.restricoes = arestas
+        self.restricoes = arestas.restricoes()
+        self.restricoes_basicas = arestas.restricoes_basicas()
     
-    def dict_format(self):
+    def dict_format(self, basico=False):
+        if not basico:
+            restricoes = self.restricoes
+        else:
+            restricoes = self.restricoes_basicas
         dicionario = {}
         for i in self.elementos:
             dicionario[i] = set()
             for j in self.elementos:
-                if (i, j) in self.restricoes or (j, i) in self.restricoes:
+                if (i, j) in restricoes or (j, i) in restricoes:
                     dicionario[i].add(j)
         return dicionario
     
     def get_vertices(self):
         return self.elementos
+    
+    def disciplinas_dia_fixo(self):
+        return [d for d in self.elementos if not d.pode_mudar]
 
-    def coloracao(self):
+    def disciplinas_mutaveis(self):
+        return [d for d in self.elementos if d.pode_mudar]
+
+    def salvar_coloracao(self):
         return {i:i.cor for i in self.elementos if i.cor is not None}
     
+    def restaurar_coloracao(self, coloracao:dict[Disciplina: int]):
+        for v in self.get_vertices():
+            if v in coloracao:
+                v.set_cor(coloracao[v])
+            else: v.set_cor(None)
+        return self
+
     def calendario(self):
-        cores = self.coloracao()
+        cores = self.salvar_coloracao()
         calendario = {}
-        for i in cores:
-            calendario[i] = [d for d in self.elementos if d.cor == i]
-            return calendario
+        for i in cores.values():
+            if i not in calendario:
+                calendario[i] = [d for d in self.elementos if d.cor == i]
+        return dict(sorted(calendario.items()))
     
-    def coloracao_inicial(self, blocos_iniciais):
+    def colorir_blocos_iniciais(self, blocos):
         i = 0
-        coloracao = {}
-        for bloco in blocos_iniciais:
+        cores_blocos = {}
+        for bloco in blocos:
             for materia in bloco:
-                coloracao[materia] = i
-            i+1
-        return coloracao
+                if materia in self.disciplinas_dia_fixo():
+                    cores_blocos[bloco] = materia.get_cor()
+        for bloco in blocos:
+            if bloco not in cores_blocos:
+                while i in cores_blocos.values():
+                    i += 1
+                cores_blocos[bloco] = i
+            for disciplina in bloco:
+                if disciplina in self.disciplinas_mutaveis():
+                    disciplina.set_cor(cores_blocos[bloco])
+        
+        while self.coloracao_invalida():
+            i=0
+            for k, j in self.coloracao_invalida():
+                if k.pode_mudar:
+                    while i in [c.get_cor() for c in self.get_vizinhos(k)]:
+                        i+=1
+                    k.set_cor(i)
+                elif j.pode_mudar:
+                    while i in [c.get_cor() for c in self.get_vizinhos(j)]:
+                        i+=1
+                    j.set_cor(i)
+        return self.calendario()
     
+    def pode_colorir(self, disciplina: Disciplina, dia: int) -> bool:
+        """
+        checa se pode designar esse dia para a disciplina
+        """
+        for vizinho in grafo.get_vizinhos(disciplina):
+            if vizinho.get_cor() == dia:
+                return False
+        return True
+
+    def reduzir_calendario(self, nr_dias_alvo = 7, max_loops = 10000):
+        dict_cores = dict(sorted(self.calendario().items(), key=lambda item: len(item[1])))
+        nr_loops = 0
+        
+        while len(self.calendario()) > nr_dias_alvo and nr_loops < max_loops - 1:
+            for bloco in dict_cores.values():
+                for materia in bloco:
+                    if materia in self.disciplinas_mutaveis():
+                        i = 0
+                        while i in [c.get_cor() for c in self.get_vizinhos(materia)]:
+                            i+=1
+                        if i < nr_dias_alvo:
+                            materia.set_cor(i)
+                        nr_loops += 1
+        return self.calendario()
+    
+
+    def coloracao_invalida(self) -> bool:
+        duplas_invalidas = []
+        for materia in self.get_vertices():
+            for vizinho in self.get_vizinhos(materia):
+                if vizinho.get_cor() != None and vizinho.get_cor() == materia.get_cor():
+                    duplas_invalidas.append((vizinho, materia))
+        return duplas_invalidas
+
     def sort_vertices_grau(self):
         for i in range(len(self.elementos)):
             for j in range(i + 1, len(self.elementos)):
@@ -194,11 +253,84 @@ class Grafo:
             if vizinho.cor is not None:
                 saturacao +=1
         return saturacao
+    
+    def pares_ruins(self):
+        pares_ruins = []
+        for dia in self.calendario():
+            if (dia - 1) not in self.calendario():
+                continue
+            for materia in self.calendario()[dia]:
+                if not materia.get_dificil():
+                    continue
+                for vizinho in grafo.dict_format(basico=True)[materia]:
+                    if vizinho in self.calendario()[dia - 1]:
+                        pares_ruins.append((vizinho, materia))
+        return pares_ruins
+    
+    def ordem_ruindade(self):
+        return len(self.pares_ruins())
+    
+    def ruindade_menor_igual(self, vertice, cor_nova):
+        cor_atual = vertice.get_cor()
+        k = self.ordem_ruindade()
+        vertice.set_cor(cor_nova)
+        j = self.ordem_ruindade()
+        vertice.set_cor(cor_atual)
+        return k >= j
+
+    def MRNA_ruindade(self, max_iteracoes_inocuas = 100, nr_dias_max = 7):
+        contador = 0
+        while self.ordem_ruindade() > 0:
+            pares = self.pares_ruins()
+            if not pares:
+                break
+            random.shuffle(pares)
+            melhoria_global = False
+            for vizinho, materia in pares:
+                ruindade_previa = self.ordem_ruindade()
+                mudou_cor = False
+                dias_tentativa = list(range(nr_dias_max))
+                random.shuffle(dias_tentativa)
+                if vizinho.pode_mudar:
+                    cor_original = vizinho.get_cor
+                    for i in dias_tentativa:
+                        if i == cor_original: continue
+                        # Checa validade (Hard) e se melhora/mantém a ruindade (Soft)
+                        if self.pode_colorir(vizinho, i) and self.ruindade_menor_igual(vizinho, i):
+                            vizinho.set_cor(i)
+                            mudou_cor = True
+                            break # Conseguiu mover, para de procurar cor para este vértice               
+                if not mudou_cor and materia.pode_mudar:
+                    cor_original = materia.get_cor()
+                    for i in dias_tentativa:
+                        if i == cor_original: continue
+                        
+                        if self.pode_colorir(materia, i) and self.ruindade_menor_igual(materia, i):
+                            materia.set_cor(i)
+                            mudou_cor = True
+                            break
+                if self.ordem_ruindade() < ruindade_previa:
+                    contador = 0
+                    melhoria_global = True
+                    break
+            if not melhoria_global:
+                contador += 1
+            
+            if contador > max_iteracoes_inocuas:
+                break            
+        return self.calendario(), self.ordem_ruindade()     
+
+    def __str__(self):
+        string = ""
+        dic = self.dict_format()
+        for i in dic:
+            string.join(str(i) + " " + str(dic[i]))
+        return string
 
 def limpar_nome(texto):
     # Normaliza, remove acentos (encode ascii), volta pra string e troca espaço por _
     return unicodedata.normalize('NFKD', texto).encode('ASCII', 'ignore').decode('utf-8').lower().replace(' ', '_')
-    
+
 def carregar_disciplinas_do_arquivo(caminho_arquivo: str):
 
     """
@@ -233,6 +365,7 @@ def carregar_disciplinas_do_arquivo(caminho_arquivo: str):
         horario = str(row['horario'])
         nome_variavel = str(row['nome_variavel']).strip()
         tem_prova = bool(row['tem_prova'])
+        eh_dificil = bool(row['eh_dificil'])
 
         if pd.notna(row['data_prof_pediu']):
             data_prof_pediu = int(row['data_prof_pediu'])
@@ -274,7 +407,8 @@ def carregar_disciplinas_do_arquivo(caminho_arquivo: str):
             periodos=periodos_list,
             curso=curso_arg,
             tem_prova=tem_prova,
-            pre_requerimento_data=data_prof_pediu
+            pre_requerimento_data=data_prof_pediu,
+            eh_dificil=eh_dificil
         )
         
         # *** A MÁGICA ***
@@ -292,6 +426,8 @@ carregar_disciplinas_do_arquivo(file_path)
 
 nr_dias = 7
 
+
+#TODO: Implementar importação de arquivos do Excel da Claudinha
 alunos_puxando = { # set com listas de todas as disciplinas que o aluno com schedule incomum está puxando
 (AL, AR, MD, LP, CVV, AEDV, EMD),
 (AL, AR, MD, LP, CVV, MFF),
@@ -302,8 +438,15 @@ alunos_puxando = { # set com listas de todas as disciplinas que o aluno com sche
 # Criação do Grafo
 ################################################################################
 restricoes = Restricoes(Disciplina.disciplinas_prova(), alunos_puxando)
-grafo = Grafo(Disciplina.disciplinas_prova(), restricoes.restricoes())
-drafo = grafo.dict_format()
-for d in drafo:
-    print("*", d, ":", drafo[d])
+grafo = Grafo(Disciplina.disciplinas_prova(), restricoes)
+
+################################################################################
+# Criação do calendário
+################################################################################
+grafo.colorir_blocos_iniciais(Disciplina.blocos_validos())
+
+print(grafo.reduzir_calendario())
+
+print(grafo.MRNA_ruindade())
+
 
--- src/algoritmos.py
+++ src/algoritmos.py
@@ -10,72 +10,46 @@ Nesse projeto, serão avaliados os seguintes algoritmos:
 
 import copy
 from Disciplinas import Disciplina, Grafo, grafo
-   
-def busca_backtraking(grafo: Grafo, cores: dict[int: list[Disciplina]] = None, num_coloridos: int = 0, num_cores_encontrado=None, coloracao_encontrada=None):
-    num_vertices = len(grafo.get_vertices())
-    if num_cores_encontrado == None: num_cores_encontrado = num_vertices + 1
-    if cores == None: cores = {i: [] for i in range(len(grafo.get_vertices()))}
-    if num_coloridos == 0:
-        for v in grafo.get_vertices():
-            if v.get_cor() != None: num_coloridos += 1
+import random
+from collections import deque
 
-    
-    max_cor = -1
-    for cor in [vert.get_cor() for vert in grafo.get_vertices()]:
-        if cor != None:
-            if cor > max_cor: 
-                if cor + 1 >= num_cores_encontrado:
-                    return (num_cores_encontrado, coloracao_encontrada)
-                max_cor = cor
-
-    if num_coloridos == num_vertices:
-        return (max_cor + 1, cores) # já se sabe que max_cor + 1 < num_cores_encontrado devido ao loop anterior
-    
-    v = None
-    for vertice in grafo.get_vertices():
-        if vertice.get_cor() == None:
-            v = vertice
-            break
-
-    blocked_colors = []
-
-    for u in grafo.get_vizinhos(v):
-        if u.get_cor() != None:
-            blocked_colors.append(u.get_cor())
-
-    # sozinho = False
-    for i in range(max_cor + 2):
-        if i >= num_vertices: break
-        # if sozinho and cores[i] == []:
-        #     continue
-        if i in blocked_colors:
-            continue
-        v.set_cor(i)
-        cores[i].append(v)
-        num_cores, coloracao = busca_backtraking(grafo, cores, num_coloridos + 1, num_cores_encontrado, coloracao_encontrada)
-        if num_cores < num_cores_encontrado:
-            num_cores_encontrado = num_cores
-            coloracao_encontrada = copy.deepcopy(coloracao)
-        cores[i].pop()
-        v.set_cor(None)
-
-        # if sozinho == False and cores[i] == []:
-        #     sozinho = True
-    
-    return (num_cores_encontrado, coloracao_encontrada)
-
-def busca_BTSL(grafo: Grafo, cores: dict[int: list[Disciplina]] = None, num_coloridos: int = 0, num_cores_encontrado=None, coloracao_encontrada=None):
-    grafo.sort_vertices_grau()
-    return busca_backtraking(grafo, cores, num_coloridos, num_cores_encontrado, coloracao_encontrada)
-
-def busca_BTDSATUR(grafo: Grafo, cores: dict[int: list[Disciplina]] = None, num_coloridos: int = 0, num_cores_encontrado=None, coloracao_encontrada=None):
+def busca_BTDSATUR(grafo: Grafo, cores: dict[int: list[Disciplina]] = {}, num_coloridos: int = 0, num_cores_encontrado=None, coloracao_encontrada={}):
+    coloracao_inicial = grafo.salvar_coloracao()
     num_vertices = len(grafo.get_vertices())
     if num_coloridos == 0:
         for v in grafo.get_vertices():
-            if v.get_cor() != None: num_coloridos += 1
+            if not v.pode_mudar: 
+                num_coloridos += 1
+        if coloracao_encontrada == {} and num_coloridos != 0: 
+            coloracao_encontrada = {i: [] for i in range(len(grafo.get_vertices()))}
+            for cor, vertices in grafo.calendario().items():
+                for u in vertices:
+                    if u.pode_mudar or (u.cor == cor):    
+                        coloracao_encontrada[cor].append(u)
+                    else: 
+                        coloracao_encontrada = {}
+
+            coloracao_num_vertices = 0
+            for cor, vertices in coloracao_encontrada.items():
+                if len(vertices) > 0:
+                    coloracao_num_vertices += len(vertices)
+                    if num_cores_encontrado == None or cor > num_cores_encontrado: 
+                        num_cores_encontrado = cor
+
+            if coloracao_num_vertices != num_vertices: 
+                coloracao_encontrada = {}
+                num_cores_encontrado = num_vertices + 1
+            else:
+                num_cores_encontrado += 1
+        
+        if cores == {} and num_coloridos != 0: 
+            cores = {i: [] for i in range(len(grafo.get_vertices()))}
+            for u in grafo.get_vertices():
+                if not u.pode_mudar and not u in cores[cor]:
+                    cores[u.cor].append(u)
 
     if num_cores_encontrado == None: num_cores_encontrado = num_vertices + 1
-    if cores == None: cores = {i: [] for i in range(len(grafo.get_vertices()))}
+    if cores == {}: cores = {i: [] for i in range(len(grafo.get_vertices()))}
     
     max_cor = -1
     for v in grafo.get_vertices():
@@ -91,7 +65,7 @@ def busca_BTDSATUR(grafo: Grafo, cores: dict[int: list[Disciplina]] = None, num_
 
     v = None
     for u in grafo.get_vertices():
-        if u.get_cor() == None:
+        if u.get_cor() == None and u.pode_mudar:
             if v == None:
                 v = u
             elif grafo.get_saturacao(u) > grafo.get_saturacao(v):
@@ -123,78 +97,9 @@ def busca_BTDSATUR(grafo: Grafo, cores: dict[int: list[Disciplina]] = None, num_
 
         # if sozinho == False and cores[i] == []:
         #     sozinho = True
-    
+    grafo.restaurar_coloracao(coloracao_inicial)
     return (num_cores_encontrado, coloracao_encontrada)
-
-def busca_gulosa_seq(grafo: Grafo, cores: dict[int: list[Disciplina]] = None, num_coloridos: int = 0):
-    if cores == None: cores = {i: [] for i in range(len(grafo.get_vertices()))}
-    grafo.sort_vertices_grau()
-    num_vertices = len(grafo.get_vertices())
-    if num_coloridos == 0:
-        for v in grafo.get_vertices():
-            if v.get_cor() != None: num_coloridos += 1
-
-    v = None
-    for vert in grafo.get_vertices():
-        if vert.get_cor() == None:
-            v = vert
-            break
-    
-    disponivel = list(range(num_vertices))
-    for u in grafo.get_vizinhos(v):
-        if u.get_cor() in disponivel:
-            disponivel.remove(u.get_cor())
-    
-    v.set_cor(disponivel[0])
-    cores[disponivel[0]].append(v)
-
-    if num_coloridos + 1 == num_vertices:
-        qtd_cor = 0
-        for cor in cores.values():
-            if len(cor) > 0:
-                qtd_cor += 1
-
-        return (qtd_cor, cores)
-    
-    return busca_gulosa_seq(grafo, cores, num_coloridos + 1)
-
-def busca_DSATUR(grafo: Grafo, cores: dict[int: list[Disciplina]] = None):
-    if cores == None: cores = {i: [] for i in range(len(grafo.get_vertices()))}
-    num_vertices = len(grafo.get_vertices())
-    grafo.sort_vertices_grau()
-    v = grafo.get_vertices()[0]
-    v.set_cor(0)
-    cores[0].append(v)
-
-    for _ in range(1, num_vertices):
-        u = None
-        for w in grafo.get_vertices():
-            if w.get_cor() == None:
-                if u == None:
-                    u = w
-                elif grafo.get_saturacao(u) < grafo.get_saturacao(w):
-                    u = w
-                elif grafo.get_saturacao(u) == grafo.get_saturacao(w):
-                    if grafo.get_grau(u) < grafo.get_grau(w):
-                        u = w
-
-        disponivel = list(range(num_vertices))
-        for w in grafo.get_vizinhos(u):
-            if w.get_cor() in disponivel:
-                disponivel.remove(w.get_cor())
-
-        u.set_cor(disponivel[0])
-        cores[disponivel[0]].append(u)
-
-
-    qtd_cor = 0
-    for cor in cores.values():
-        if len(cor) == 0:
-            break
-        qtd_cor += 1
-
-    return (qtd_cor, cores)
-    
+   
 def limpar_coloracao(coloracao):
     nova_coloracao = {}
     for cor, vertices in coloracao.items():
@@ -209,7 +114,9 @@ def encher_coloracao(coloracao, tam_grafo):
         coloracao[i] = []
     return coloracao
 
-if __name__ == '__main__':
+
+
+if __name__ == '__main_':
 
     print(grafo)
 
@@ -222,21 +129,24 @@ if __name__ == '__main__':
     coloracao = limpar_coloracao(coloracao)
     print(f'Número de cores necessário: {num_cores}')
     print(f'Coloração:\n{coloracao}')
-    for v in grafo.get_vertices():
-        v.set_cor(None)
 
     print('\nBusca com um algorítmo DSATUR:')
     num_cores, coloracao = busca_DSATUR(grafo, copy.deepcopy(cores))
     coloracao = limpar_coloracao(coloracao)
     print(f'Número de cores necessário: {num_cores}')
     print(f'Coloração:\n{coloracao}')
-    for v in grafo.get_vertices():
-        v.set_cor(None)
+
 
     print('\nBusca com um algorítmo de backtracking:')
     num_cores, coloracao = busca_backtraking(grafo, copy.deepcopy(cores), 0, num_cores, encher_coloracao(coloracao, len(grafo.get_vertices())))
     coloracao = limpar_coloracao(coloracao)
     print(f'Número de cores necessário: {num_cores}')
     print(f'Coloração:\n{coloracao}')
-    for v in grafo.get_vertices():
-        v.set_cor(None)
+
+
+    print(grafo.salvar_coloracao())
+
+if __name__ == '__main__':
+    num, col = busca_BTDSATUR(grafo)
+
+    print(num, col)
